694d2e7310691fe397d93c1139074de50d9bed1a
LizzBizzLol <150434567+LizzBizzLol@users.noreply.github.com>
2024-11-28 21:45:11 +0500

    Update neo4j_utils.py

--- neo4j_utils.py
+++ neo4j_utils.py
@@ -10,15 +10,22 @@ class Neo4jHelper:
     def add_user(self, user_id, screen_name, name, sex, home_town):
         with self.driver.session() as session:
             session.run(
-                "MERGE (u:User {id: $id}) "
-                "SET u.screen_name = $screen_name, u.name = $name, u.sex = $sex, u.home_town = $home_town",
+                """
+                MERGE (u:User {id: $id})
+                SET u.screen_name = $screen_name, 
+                    u.name = $name, 
+                    u.sex = $sex, 
+                    u.home_town = $home_town
+                """,
                 id=user_id, screen_name=screen_name, name=name, sex=sex, home_town=home_town
             )
 
     def add_relationship(self, user1_id, user2_id, relation_type):
         with self.driver.session() as session:
             session.run(
-                "MATCH (u1:User {id: $user1_id}), (u2:User {id: $user2_id}) "
-                f"MERGE (u1)-[:{relation_type}]->(u2)",
+                """
+                MATCH (u1:User {id: $user1_id}), (u2:User {id: $user2_id})
+                MERGE (u1)-[:{relation_type}]->(u2)
+                """.replace("{relation_type}", relation_type),
                 user1_id=user1_id, user2_id=user2_id
             )
